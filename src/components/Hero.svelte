433aef6be115722bb4c2fb27eb02a0528617ba87
RobAntunes <robm.antunes@gmail.com>
2025-04-25 15:06:48 +0200

    ready for meeting 1

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -9,12 +9,12 @@
 <section
   class="p-12 pt-0 bg-[whitesmoke] h-screen w-screen flex justify-center items-center"
 >
-  <div class="relative w-full h-full max-w-7xl max-h-[80vh] rounded-3xl">
+  <div class="relative h-full max-h-[80vh] rounded-3xl">
     <!-- Image takes full container size and is positioned absolutely -->
     <img
       src={hero.src}
       alt="hero"
-      class="absolute inset-0 w-full h-full object-cover rounded-3xl bg-blend-multiply"
+      class="absolute inset-0 w-full h-full object-cover rounded-3xl"
     />
 
     <!-- Top-left Logo Box -->
@@ -40,94 +40,90 @@
 
     <!-- Main Text Area -->
     <div
-      class="absolute h-full pb-12 left-12 text-white drop-shadow-2xl flex flex-col gap-4 justify-end flex-grow-1"
+      class="h-full pl-12 text-white relative z-50 flex flex-col gap-4 justify-end"
     >
-      <h2 class="text-[96px] leading-tight font-bold">
-        Compliance,<br />meet Velocity
-      </h2>
-      <h3 class="text-[18px] leading-normal font-light max-w-xl">
-        See Risks Before They Happen. Act Smarter Every Day.
-      </h3>
-      <div class="flex justify-start items-center space-x-4 mt-4">
-        <Button
-          text="Get Started"
-          variant="primary"
-          size={{ height: "50px", width: "160px" }}
-        />
-        <Button
-          text="Learn More"
-          variant="ghost"
-          size={{ height: "50px", width: "160px" }}
-          bgColor="blue-700"
-          hoverColor="blue-700"
-          textColor="white"
-        />
-      </div>
-    </div>
-
-    <div
-      class="flex bottom-0 right-0 text-white justify-end items-end w-full h-full"
-    >
-      <p
-        class=" text-white drop-shadow-2xl font-regular text-md relative z-50 p-4 bottom-1 w-1/4 text-balance leading-normal h-full flex items-end flex-wrap break-words"
-      >
-        Streamlining tax compliance for complex regulatory environments.
-      </p>
-      <!-- Bottom-right Box -->
-      <div
-        class="flex flex-col justify-end items-end bottom-0 right-0 w-1/4 h-[40%] bg-[whitesmoke] isolate rounded-tl-2xl"
-      >
-        <div class="flex order-1 justify-start items-start">
-          <img
-            src={inverse.src}
-            alt="decoration"
-            class="z-50 right-0 top-0 rotate-[180deg] translate-y-[101%]"
-          />
+      <div class="grid grid-cols-12 items-end h-full">
+        <div class="col-span-6 flex flex-col justify-end row-start-2 pb-12">
+          <h2 class="text-[96px] leading-tight font-bold">
+            Compliance,<br />meet Velocity
+          </h2>
+          <h3 class="text-[18px] leading-normal font-light max-w-xl">
+            See Risks Before They Happen. Act Smarter Every Day.
+          </h3>
+          <div class="flex justify-start items-center space-x-4 mt-4">
+            <Button
+              text="Get Started"
+              variant="primary"
+              size={{ height: "50px", width: "160px" }}
+            />
+            <Button
+              text="Learn More"
+              variant="ghost"
+              size={{ height: "50px", width: "160px" }}
+              bgColor="blue-700"
+              hoverColor="blue-700"
+              textColor="white"
+            />
+          </div>
         </div>
-        <div class="flex order-3 justify-start items-end w-full">
+        <!-- <div class="flex items-end col-start-7 col-span-3 row-start-2 pb-12">
+          <p class="text-balance ml-12 text-white">
+            Streamlining tax compliance for complex regulatory environments.
+          </p>
+        </div> -->
+        <div
+          class="row-span-2 col-start-9 flex justify-end items-end relative top-[1px]"
+        >
           <img
             src={inverse.src}
             alt="decoration"
-            class="z-50 left-0 bottom-0 translate-x-[-99%] rotate-[180deg] translate-y-[1%]"
+            class="z-50 flex justify-end items-end rotate-[180deg] relative"
           />
         </div>
         <div
-          class=" relative flex order-2 justify-center items-center w-full rounded-tl-xl h-full"
+          class="flex flex-row text-white justify-end items-end w-full col-span-3 col-start-10 row-start-2"
         >
-          <div
-            class="flex justify-center items-center absolute bg-[whitesmoke] top-5 right-1 rounded-full h-[60px] w-[60px]"
-          >
-            <div class="bg-blue-700 text-[whitesmoke] text-md rounded-full p-2">
-              <ArrowRight
-                class="hover:cursor-pointer hover:scale-110 transition-all duration-300 hover:translate-x-1"
-              />
-            </div>
-          </div>
-          <div
-            class="grid grid-cols-2 grid-rows-3 justify-around items-center p-3 bg-blue-700 rounded-xl h-[90%] w-full mx-3 mt-6"
-          >
-            <!-- <div class="flex order-1 justify-center items-start">
+          <div class="flex flex-col">
+            <div class="flex justify-end items-end relative top-[1px]">
               <img
                 src={inverse.src}
                 alt="decoration"
-                class="z-50 right-0 top-0 rotate-[180deg] translate-y-[-99%]"
+                class="z-50 flex justify-end items-end rotate-[180deg] relative"
               />
             </div>
-            <div class="flex order-3 justify-start items-end w-full">
-              <img
-                src={inverse.src}
-                alt="decoration"
-                class="z-50 left-0 bottom-0 translate-x-[-99%] rotate-[180deg]"
-              />
-            </div> -->
-            <p class="text-[whitesmoke] gap-y-4 text-[16px] text-balance col-span-2">
-              Find out how Taxera can help you achieve compliance in record time, with a few clicks.
-            </p>
-            <img
-              src={tax.src}
-              alt="tax"
-              class="object-cover w-full h-6/7 rounded-lg row-start-2 col-span-2 row-span-2"
-            />
+            <!-- Bottom-right Box -->
+            <div
+              class="relative z-40 flex flex-col justify-end items-end w-full bg-[whitesmoke] rounded-tl-2xl pt-3 pl-3"
+            >
+              <div
+                class="flex justify-center items-center absolute top-0 -right-3 bg-[whitesmoke] rounded-full h-[60px] w-[60px] z-[60]"
+              >
+                <div
+                  class="bg-blue-700 text-[whitesmoke] text-md rounded-full p-2"
+                >
+                  <ArrowRight
+                    class="hover:cursor-pointer hover:scale-110 transition-all duration-300 hover:translate-x-1"
+                  />
+                </div>
+              </div>
+              <div
+                class="flex flex-col justify-center items-center rounded-tl-2xl relative z-50"
+              >
+                <div
+                  class="flex order-2 justify-end items-center rounded-tl-xl"
+                >
+                  <div
+                    class="flex flex-col gap-4 p-3 pb-0 bg-blue-700 rounded-xl"
+                  >
+                    <p class="text-[whitesmoke] text-[16px] text-balance">
+                      Find out how Taxera can help you achieve compliance in
+                      record time, with a few clicks.
+                    </p>
+                    <img src={tax.src} alt="tax" class="rounded-xl pb-3" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
